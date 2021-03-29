9ee86c4c2ed4cdc4b10cec76e08417771acd2cdb
Robert Schuh <github@eneticum.de>
2021-03-29 16:28:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const ExpoRandom = require("expo-random");
 
 export function randomBytes (length, cb) {
   if (!cb) {
-    return ExpoRandom.getRandomBytes(length);
+    return Buffer.concat(
+      new Array(Math.ceil(length / 1024))
+        .fill(undefined)
+        .map((e, i) => ExpoRandom.getRandomBytes(Math.min(1024, length - (i * 1024))))
+    );
   }
-  ExpoRandom.getRandomBytesAsync(length).then(res => cb(null, res)).catch(e => cb(e));
+  return Promise.all(
+    new Array(Math.ceil(length / 1024))
+      .fill(undefined)
+      .map((e, i) => ExpoRandom.getRandomBytesAsync(Math.min(1024, length - (i * 1024))))
+  ).then(list => Buffer.concat(list))
+    .then(res => cb(null, res))
+    .catch(e => cb(e));
 }
 
